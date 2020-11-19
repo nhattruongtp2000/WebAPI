d145c950f54871790ad40bc848f21db7bd53c0b9
Truong <nhattruongtp2000@gmail.com>
2020-11-19 11:30:16 +0700

    view-bag-user

--- WebAPI.AdminApp/Controllers/UserController.cs
+++ WebAPI.AdminApp/Controllers/UserController.cs
@@ -38,7 +38,12 @@ namespace WebAPI.AdminApp.Controllers
                 PageSize = pageSize
             };
             var data = await _userApiClient.GetUsersPagings(request);
-            ViewBag.Keyword = keyword;  
+            ViewBag.Keyword = keyword;
+            if (TempData["result"] != null)
+            {
+                ViewBag.SuccessMsg = TempData["result"];
+            }
+
             return View(data.ResultObj);
         }
 
@@ -61,7 +66,10 @@ namespace WebAPI.AdminApp.Controllers
 
             var result = await _userApiClient.RegisterUser(request);
             if (result.IsSuccessed)
+            {
+                TempData["result"] = "Thêm mới người dùng thành công";
                 return RedirectToAction("Index");
+            }
 
             ModelState.AddModelError("", result.Message);
             return View(request);
@@ -105,7 +113,10 @@ namespace WebAPI.AdminApp.Controllers
 
             var result = await _userApiClient.UpdateUser(request.Id, request);
             if (result.IsSuccessed)
+            {
+                TempData["result"] = "Cập nhật người dùng thành công";
                 return RedirectToAction("Index");
+            }
 
             ModelState.AddModelError("", result.Message);
             return View(request);
